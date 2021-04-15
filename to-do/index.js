022916d11db89dd31fc9ebc0fec3e132b7a93521
Zimnitski Ihar <zimnitski.ihar@gmail.com>
2021-04-15 13:48:32 +0300

    refactor: rewrite to class

--- to-do/index.js
+++ to-do/index.js
@@ -1,56 +1,91 @@
-const taskList = document.getElementById('task-list');
-const newTaskArea = document.getElementById('new-task__area');
-const addTaskBtn = document.getElementById('add-btn');
-
-const TASKS = [];
-
-taskList.addEventListener('click', markTask);
-addTaskBtn.addEventListener('click', addTask);
-
-function markTask(evt) {
-  const e = evt;
-
-  if (e.target.classList.contains('task')) {
-    const done = e.target.dataset.done === 'true' ? true : false;
-    e.target.dataset.done = !done;
+class ToDo {
+  constructor(list) {
+    this.list = list;
+    this.tasks = [];
+    this.checkLocal();
+    document.getElementById('add-btn').addEventListener('click', this.addTaskHandler);
+    this.list.addEventListener('click', this.taskHandler);
   }
 
-  if (e.target.classList.contains('mark-btn')) {
-    const important = e.target.closest('.task').dataset.important === 'true' ? true : false;
+  checkLocal() {
+    const localList = localStorage.getItem('taskList');
 
-    e.target.classList.toggle('mark-btn_important');
-    e.target.closest('.task').dataset.important = !important;
+    if (localList) this.renderList(localList);
   }
-}
 
-function addTask() {
-  const { value } = newTaskArea;
+  renderList(tasks) {
+    this.list.innerHTML = '';
 
-  const task = {
-    value,
-    important: false,
-    done: false,
+    tasks.forEach((el) => {
+      this.renderTask(el);
+    });
+  }
+
+  renderTask({ value, important, done, id }) {
+    const btnImportant = important ? '' : 'mark-btn_important';
+
+    const html = `
+      <li class="task" data-important=${important} data-done=${done} data-id=${id}>
+        <div class="task-btn__wrapper">
+          <button class="mark-btn ${btnImportant}">Mark important</button>
+          <button class="delete-btn"></button>
+        </div>
+        <div class="star"></div>
+        <span class="task__text">${value}</span>
+      </li>`;
+
+    this.list.insertAdjacentHTML('beforeend', html);
+  }
+
+  createTask(value) {
+    const task = {
+      value,
+      important: false,
+      done: false,
+      id: Date.now(),
+    };
+
+    this.renderTask(task);
+
+    this.tasks.push(task);
+  }
+
+  addTaskHandler = () => {
+    const taskArea = document.getElementById('new-task__area');
+    const { value } = taskArea;
+    this.createTask(value);
+    taskArea.value = '';
   };
 
-  createTask(task);
+  taskHandler = (e) => {
+    if (e.target.classList.contains('task')) {
+      const done = e.target.dataset.done === 'true' ? true : false;
+      e.target.dataset.done = !done;
+    }
 
-  newTaskArea.value = '';
+    if (e.target.classList.contains('mark-btn')) {
+      const important = e.target.closest('.task').dataset.important === 'true' ? true : false;
 
-  TASKS.push(task);
+      e.target.classList.toggle('mark-btn_important');
+      e.target.closest('.task').dataset.important = !important;
+    }
+
+    if (e.target.classList.contains('delete-btn')) {
+      this.deleteTask(e);
+    }
+  };
+
+  deleteTask = (evt) => {
+    const { id } = evt.target.closest('.task').dataset;
+
+    const index = this.tasks.findIndex((el) => el.id === +id);
+
+    this.tasks.splice(index, 1);
+
+    this.renderList(this.tasks);
+  };
 }
 
-function createTask({ value, important, done }) {
-  const btnImportant = important ? '' : 'mark-btn_important';
+const taskList = document.getElementById('task-list');
 
-  const html = `
-    <li class="task" data-important=${important} data-done=${done}>
-      <div class="task-btn__wrapper">
-        <button class="mark-btn ${btnImportant}">Mark important</button>
-        <button class="delete-btn"></button>
-      </div>
-      <div class="star"></div>
-      <span class="task__text">${value}</span>
-    </li>`;
-
-  taskList.insertAdjacentHTML('beforeend', html);
-}
+const toDo = new ToDo(taskList);
